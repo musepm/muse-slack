ac05033596255064f39d3d45eb57aac4aa59add4
Jason Livesay <ithkuil@gmail.com>
2015-05-29 03:32:45 +0000

    mv to bin

---
+++ bin/signup.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+'use strict';
+
+var rlp = require('readline-prompter');
+
+var su = '\n\n---------------------------------------------------------------\n\nPlease visit https://slack.com to sign up.\nAfter signing up, click the activation link in the email.\nFollow the instructions to create a team.\n\nThen create a new bot integration at https://my.slack.com/services/new/bot\n\nCopy and paste your token below:\n';
+console.log(su);
+
+rlp(['Token'], {}, {}).end(function (results) {
+
+  require('muse').newCredentials({ 'slack': { 'token': results.token } });
+});
