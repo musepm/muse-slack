a6d56fab6f7c0fc06909942d9cd7557f69439eee
Jason Livesay <ithkuil@gmail.com>
2015-05-29 01:25:56 +0000

    fix

--- src/signup.js
+++ src/signup.js
@@ -1,8 +1,6 @@
 
 
-module.exports = {
-  signup() {
-    let su = `
+let su = `
 Please visit https://slack.com to sign up.
 After signing up, click the activation link in the email.
 Follow the instructions to create a team.
@@ -11,6 +9,4 @@ Then create a new bot integration at https://my.slack.com/services/new/bot
 
 Copy and paste your token here`
    
-    console.log(su);
-  }
-}
+console.log(su);
