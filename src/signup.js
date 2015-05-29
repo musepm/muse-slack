b641ddddb1667623668097d3e791a22d8da75f61
Jason Livesay <ithkuil@gmail.com>
2015-05-29 02:24:53 +0000

    update

--- src/signup.js
+++ src/signup.js
@@ -1,12 +1,22 @@
 
-
 let su = `
+
+---------------------------------------------------------------
+
 Please visit https://slack.com to sign up.
 After signing up, click the activation link in the email.
 Follow the instructions to create a team.
 
 Then create a new bot integration at https://my.slack.com/services/new/bot
 
-Copy and paste your token here`
-   
+Copy and paste your token below:
+`
 console.log(su);
+
+require('readline-prompter')(['Token'], {}, {}, results => {
+
+  require('muse').newCredentials(
+    {  'slack': { 'token': results.token } }
+  );
+
+});
