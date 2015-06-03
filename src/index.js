3850bfe9413ff1aceb2a119d8cb623b571babb58
Jason Livesay <ithkuil@gmail.com>
2015-06-03 19:50:19 +0000

    works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 var muse = require('musepm'),
-    SlackClient = require('node-slack-client'),
+    SlackClient = require('slack-client'),
     credentials = require('musepm-credentials');
 
 module.exports = {
   async signon(cfg) {
-    let creds = await credentials.getAll();
-    var slack = new SlackClient(creds[cfg], true, true);
+    try {
+      var creds = await credentials.getAll('slack');
+      var slack = new SlackClient(creds[cfg], true, true);
+    } catch (e) {
+      console.error(e);
+    }
+    slack.login();
     return slack;
   }
 }
