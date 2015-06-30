c68426918e8de466a6f8541f80a2bc4feebde0a2
Jason Livesay <ithkuil@gmail.com>
2015-06-30 08:13:02 +0000

    save progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 var SlackClient = require('slack-client'),
-    credentials = require('musepm-credentials');
+    credentials = require('musepm-credentials'),
+    Channel = require('slack-client/src/Channel'),
+    monitor = require('musepm-monitor');
+
+monitor.logCalls('slack','send','Channel.prototype.send'
 
 module.exports = {
   async signon(cfg) {
-    try {
+    try { 
+      var cls = Channel.prototype; 
+      cls.send = monitor.logCalls('slack','send', cls.send);
       var creds = await credentials.getAll('slack');
       var slack = new SlackClient(creds[cfg], true, true);
     } catch (e) {
@@ -13,4 +19,5 @@ module.exports = {
     return slack;
   }
 }
+
 
